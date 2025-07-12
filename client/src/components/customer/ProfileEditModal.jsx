a4409bba72a366133bf8fc0e8ca4c1bf4fb05b62
kaustubh <kaustubhsoni34@gmail.com>
2025-07-12 21:27:32 +0530

    profile edit modal page

--- client/src/components/customer/ProfileEditModal.jsx
+++ client/src/components/customer/ProfileEditModal.jsx
@@ -8,17 +8,15 @@ const ProfileEditModal = ({ isOpen, onClose, oldData }) => {
   return (
     <>
       <div className="inset-0 fixed bg-black/70 flex justify-center items-center z-[101]">
-  <div className="border border-[#d6b78f] w-1/2 min-h-[70vh] mt-10 bg-[#fff7ef] rounded-2xl shadow-xl">
-    
-    <div className="text-xl flex justify-between items-center p-4 border-b-2 border-[#e5c79d] bg-[#fdeada] rounded-t-2xl">
-      <h1 className="font-bold text-[#7a1d1d]">Edit Profile</h1>
-      <button onClick={onClose}>
-        <IoIosCloseCircle className="text-3xl text-[#c04a3c] hover:text-[#a03027] transition-colors duration-200" />
-      </button>
-    </div>
-  </div>
-</div>
-
+        <div className="border border-[#d6b78f] w-1/2 min-h-[70vh] mt-10 bg-[#fff7ef] rounded-2xl shadow-xl">
+          <div className="text-xl flex justify-between items-center p-4 border-b-2 border-[#e5c79d] bg-[#fdeada] rounded-t-2xl">
+            <h1 className="font-bold text-[#7a1d1d]">Edit Profile</h1>
+            <button onClick={onClose}>
+              <IoIosCloseCircle className="text-3xl text-[#c04a3c] hover:text-[#a03027] transition-colors duration-200" />
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
