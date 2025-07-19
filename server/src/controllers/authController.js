5d82f760989504f2260ab052dd02fb0f98ec9e0f
kaustubh <kaustubhsoni34@gmail.com>
2025-07-19 21:15:53 +0530

    Implement user deactivation logic and update user profile route

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,40 +1,50 @@
 import User from "../models/userModel.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import genToken from "../utils/auth.js";
+import Deactivation from "../models/deactivationModel.js";
 
 export const RegisterUser = async (req, res, next) => {
-  try{
+  try {
     const { name, email, password, phone } = req.body;
 
-  if (!name || !email || !password || !phone) {
-    const error = new Error("All Feilds Are Required");
-    error.statusCode = (400);
-    return next(error);
-  }
+    if (!name || !email || !password || !phone) {
+      const error = new Error("All Feilds Are Required");
+      error.statusCode = 400;
+      return next(error);
+    }
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    const error = new Error("Email Already Registered");
-    error.statusCode = (409);
-    return next(error);
-  }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("Email Already Registered");
+      error.statusCode = 409;
+      return next(error);
+    }
 
-  const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  const profilePic = `https://placehold.co/600x400?text=${name.charAt(0).toUpperCase()}`
+    const profilePic = `https://placehold.co/600x400?text=${name
+      .charAt(0)
+      .toUpperCase()}`;
 
+    if (existingUser && existingUser.status === "Inactive") {
+      existingUser.name = name;
+      existingUser.password = hashedPassword;
+      existingUser.status = "Active";
+      existingUser.photo = profilePic;
+      existingUser.role = "User";
+      await existingUser.save();
+    } else {
+      const newUser = await User.create({
+        name,
+        email,
+        phone,
+        password: hashedPassword,
+        photo: profilePic,
+      });
+    }
 
-  const newUser = await User.create({
-    name,
-    email,
-    phone,
-    password:hashedPassword,
-    photo:profilePic,
-    
-  });
-
-  res.status(201).json({ message: "Registration Successfull" });
-  } catch (error){
+    res.status(201).json({ message: "Registration Successfull" });
+  } catch (error) {
     next(error);
   }
 };
@@ -64,16 +74,129 @@ export const LoginUser = async (req, res, next) => {
       return next(error);
     }
 
-    genToken(user._id, res)
-    
-    res
-      .status(200)
-      .json({message: `Welcome Back ${user.name}`,data: user});
+    genToken(user._id, res);
+
+    res.status(200).json({ message: `Welcome Back ${user.name}`, data: user });
   } catch (error) {
     next(error);
   }
 };
 
-export const LogoutUser = (req, res) => {};
+export const LogoutUser = (req, res, next) => {
+  try {
+    res.cookie("IDCard", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout Successfull" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const UpdateUser = (req, res) => {};
+export const UpdateUser = async (req, res, next) => {
+  try {
+    const currentUser = req.user;
+    const {
+      name,
+      phone,
+      gender,
+      occupation,
+      address,
+      city,
+      state,
+      district,
+      representing,
+    } = req.body;
+
+    if (!currentUser) {
+      const error = new Error("User Not Found !! Login Again");
+      error.statusCode = 401;
+      return next(error);
+    }
+    const photo = req.file;
+    let picture;
+    if (photo) {
+      const b64 = Buffer.from(photo.buffer).toString("base64");
+      const dataURI = `data:${photo.mimetype};base64,${b64}`;
+
+      const result = await cloudinary.uploader.upload(dataURI, {
+        folder: "eventPlannerPictures",
+        width: 500,
+        height: 500,
+        crop: "fill",
+      });
+      picture = result.secure_url;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      currentUser._id,
+      {
+        name,
+        phone,
+        gender,
+        occupation,
+        address,
+        city,
+        state,
+        district,
+        representing,
+        photo: picture || currentUser.photo,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({ message: "Profile Updated", data: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const currentUser = req.user;
+    const { reason, feedback, confirmPassword } = req.body;
+
+    console.log(currentUser);
+
+    console.log(reason, feedback, confirmPassword, currentUser.password);
+
+    if (!currentUser) {
+      const error = new Error("User Not Found !! Login Again");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    const isVerified = await bcrypt.compare(
+      confirmPassword,
+      currentUser.password
+    );
+
+    if (!isVerified) {
+      const error = new Error("Invalid Username or Password");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      currentUser._id,
+      {
+        gender: "N/A",
+        occupation: "N/A",
+        address: "N/A",
+        city: "N/A",
+        state: "N/A",
+        district: "N/A",
+        representing: "N/A",
+        photo: "N/A",
+        role: "N/A",
+        password: "N/A",
+        status: "Inactive",
+      },
+      { new: true }
+    );
+
+    await Deactivation.create({ userId: currentUser._id, reason, feedback });
+
+    res.status(200).json({ message: "Sorry to see you go . . ." });
+  } catch (error) {
+    next(error);
+  }
+};
