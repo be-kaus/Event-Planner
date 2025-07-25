cfc694f1dff403d44a02a59fb64452d9a8349ed9
kaustubh <kaustubhsoni34@gmail.com>
2025-07-25 19:53:55 +0530

    Implement CustomerDashboard with sidebar navigation and initial component structure; add Sidebar component

--- client/src/components/AdminPanel.jsx
+++ client/src/components/AdminPanel.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import Sidebar from "../components/Customer/Sidebar";
+import Overview from "../components/Customer/Overview";
+import Bookings from "../components/Customer/Bookings";
+import Support from "../components/Customer/Support";
+import Feedback from "../components/Customer/Feedback";
+import Profile from "./customer/Profile";
 
-const AdminPanel = () => {
+const CustomerDashboard = () => {
+  const [active, setActive] = useState("overview");
+
   return (
-    <>
-    <div>AdminPanel</div>
-    </>
-  )
-}
+    <div className="min-h-screen bg-gradient-to-b from-[#fff3e0] to-[#fbe8d3] flex">
+      
+      <Sidebar active={active} setActive={setActive} />
 
-export default AdminPanel
+      <div className="w-full p-6 md:p-10 border-l border-[#c49b63] bg-white shadow-lg overflow-y-auto">
+        {active === "overview" && <Overview />}
+        {active === "profile" && <Profile />}
+        {active === "bookings" && <Bookings />}
+        {active === "support" && <Support />}
+        {active === "feedback" && <Feedback />}
+      </div>
+    </div>
+  );
+};
+
+export default CustomerDashboard;
