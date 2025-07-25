9f6a97ad13f24b1e41b535499ff8d1703122500b
kaustubh <kaustubhsoni34@gmail.com>
2025-07-25 19:50:14 +0530

    Remove unnecessary comments in CustomerDashboard for cleaner code

--- client/src/components/CustomerDashboard.jsx
+++ client/src/components/CustomerDashboard.jsx
@@ -11,10 +11,9 @@ const CustomerDashboard = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#fff3e0] to-[#fbe8d3] flex">
-      {/* Sidebar */}
+      
       <Sidebar active={active} setActive={setActive} />
 
-      {/* Main Content Area */}
       <div className="w-full p-6 md:p-10 border-l border-[#c49b63] bg-white shadow-lg overflow-y-auto">
         {active === "overview" && <Overview />}
         {active === "profile" && <Profile />}
