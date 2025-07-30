bc1ba2223248070fae4df0d8178d8cb69c3c1b8b
kaustubh <kaustubhsoni34@gmail.com>
2025-07-30 20:05:06 +0530

    Refactor authMiddleware and sendEmail utility for improved readability and consistency

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,29 +1,31 @@
-import nodemailer from `nodemailer`
+import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, mailBody) => {
-    try{
-        const transpoter = nodemailer.createTransport({
-            service: 'gmail',
-            auth:{
-                user:process.env.GMAIL_USER,
-                pass:process.env.GMAIL_PASSCODE,
-            },
-        });
+  try {
+    const Truck = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSCODE,
+      },
+    });
 
-        const mailOptions = {
-            from: process.env.GMAIL_USER,
-            to,
-            subject,
-            html: mailBody,
-        };
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      to,
+      subject,
+      html: mailBody,
+    };
 
-        const result = await transpoter.sendMail(mailOptions);
-        console.log(`Email sent successfully to`, result.messageId);
-        return true;
-    }catch (error){
-        console.error(`Error Sending Email`,error)
-        return false;
-    }
+    const result = await Truck.sendMail(mailOptions);
+    console.log("Email Sent Successfully", result.messageId);
+    return true;
+
+  } catch (error) {
+    console.error("Error sending Email", error);
+    return false;
+  }
 };
+
 
 export default sendEmail;
